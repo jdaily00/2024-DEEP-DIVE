e0d15bf451098419653eda1bbe2053fc64f87aed
jdaily00 <kyeonghee_@naver.com>
2024-11-23 00:48:22 +0900

    에러 안 나게 수정

--- 02. JavaScript/02-vet,let,const/const.js
+++ 02. JavaScript/02-vet,let,const/const.js
@@ -2,9 +2,9 @@ const greeting = "hello";
 console.log(greeting);
 
 // 중복 선언 불가
-const greeting = "hi";
-console.log(greeting);
+// const greeting = "hi";
+// console.log(greeting);
 
 // 재할당 불가
-greeting = "how are you?";
-console.log(greeting);
+// greeting = "how are you?";
+// console.log(greeting);
--- 02. JavaScript/02-vet,let,const/let.js
+++ 02. JavaScript/02-vet,let,const/let.js
@@ -2,8 +2,8 @@ let greeting = "hello";
 console.log(greeting);
 
 // 중복 선언 불가
-let greeting = "hi";
-console.log(greeting);
+// let greeting = "hi";
+// console.log(greeting);
 
 // 재할당 가능
 greeting = "hi";
