0bd9c05e50b75064034f0a68410775fb7155d302
jdaily00 <kyeonghee_@naver.com>
2024-11-23 09:43:03 +0900

    const 배열과 객체의 값 변경 가능

--- 02. JavaScript/02-vet,let,const/const.js
+++ 02. JavaScript/02-vet,let,const/const.js
@@ -8,3 +8,12 @@ console.log(greeting);
 // 재할당 불가
 // greeting = "how are you?";
 // console.log(greeting);
+
+// 배열과 객체의 값을 변경하는 것은 가능
+const arrayList = [1, 2, 3];
+arrayList.push(4);
+console.log(arrayList);
+
+const objectList = { a: "a", b: "b" };
+objectList.c = "c";
+console.log(objectList);
